372c24f2c00ad2ebef328e326b296c11cdbe8aa5
Luciano <luchosr@gmail.com>
2019-04-16 19:20:07 -0300

    add main module

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import Nabvar from "./components/Navbar";
+import Main from "./components/main/Main";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Nabvar name="freeCodeCamp Markdown Previewer" />
+        <Main />
       </div>
     );
   }
---
+++ src/components/main/Main.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class Main extends Component {
+  render() {
+    return (
+      <main>
+        <textarea id="editor" rows="10" cols="50">
+          Hola que tal yo soy un textarea
+        </textarea>
+      </main>
+    );
+  }
+}
+
+export default Main;
