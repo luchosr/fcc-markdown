4d642cce6231354494df99b2ffe08f4c3b9a91f0
Luciano <luchosr@gmail.com>
2019-04-22 15:32:33 -0300

    add marked library

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,12 +1,41 @@
 import React, { Component } from "react";
+import marked from "marked";
 
 class Main extends Component {
+  state = {
+    markdown: ""
+  };
+
+  updateMarkdown = function(markdown) {
+    this.setState({ markdown });
+  };
+
   render() {
+    let { markdown } = this.state;
+    console.log(markdown);
+
+    //tomar el contenido del input
+    //procesarlo con marked
+    //devolverlo en el output.
+
     return (
       <main>
-        <textarea id="editor" rows="10" cols="50">
-          Hola que tal yo soy un textarea
-        </textarea>
+        <textarea
+          componentClass="textarea"
+          id="editor"
+          rows="10"
+          cols="50"
+          value={markdown}
+          onChange={event => this.updateMarkdown(event.target.value)}
+          placeholder="Escribí algo."
+        />
+        <p id="output"> Output</p>
+        <div
+          id="preview"
+          //dangerouslySetInnerHTML={{ __html: marked(markdown) }}
+        >
+          {marked(markdown)}
+        </div>
       </main>
     );
   }
